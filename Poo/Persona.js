41afee8ab0788237d9f77a39abd1d0502ad515a1
Kiskotkd <fjgp92@hotmail.com>
2023-06-29 12:51:02 +0200

    reto3

--- Poo/Persona.js
+++ Poo/Persona.js
@@ -5,23 +5,32 @@
 3. Crea una carpeta llamada POO, donde guardaremos todos los retos de hoy.
 4. Crea la clase Person de la manera más completa que se te ocurra. Su constructor debe
 tener como mínimo 2 parámetros de entrada.*/
+
+class Persona {
+    constructor(pelo, altura, sexo, nacionalidad, peso, yearOfBirth){
+        this.pelo = pelo;
+        this.altura = altura;
+        this.sexo = sexo;
+        this.nacionalidad = nacionalidad;
+        this.peso = peso;
+        this.yearOfBirth = yearOfBirth;
+    }
 /*Reto 2
 Utilizando la clase Person definida en el punto anterior, haz que se pueda calcular el IMC
 (índice de masa corporal) de cualquier persona que se haya creado como un objeto de la
 clase.*/
-class Persona {
-    constructor(pelo, altura, sexo, edad, peso){
-        this.pelo = pelo;
-        this.altura = altura;
-        this.sexo = sexo;
-        this.edad = edad;
-        this.peso = peso;
-    }
     imc(){
        return this.peso / Math.pow(this.altura, 2)* 10000;
     }
+/*Reto 3
+Añadir el atributo yearOfBirth a la clase Person e implementar el método edad que tome
+como parámetro el año y calcule la edad de la persona. */
+    edad(){
+        return 2023 - this.yearOfBirth;
+    }
 }
 
-let paco = new Persona("Rubio", 183, "M", 35, 80);
+let paco = new Persona("Rubio", 183, "M", "Griego", 80, 1985);
 console.log(paco.imc())
+console.log(paco.edad())
 
