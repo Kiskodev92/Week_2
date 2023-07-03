9d69a42e92af5a190e52ec0ed3a98fbd762816db
Kiskotkd <fjgp92@hotmail.com>
2023-07-03 13:13:25 +0200

    reto 8

--- Poo/contactest.js
+++ Poo/contactest.js
@@ -1,3 +1,7 @@
 let {Persona} = require("./Persona");
 let {Contact} = require("./contact");
 
+let pedro = new Persona ("Rubio", 189, "M", "Griego", 70, 2000,["comer", "entrenar", "andar"])
+let test = new Contact()
+test.array.push(pedro)
+test.printPersona();
