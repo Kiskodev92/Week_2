bcb9ffde6a07943f346bbc0c264029012dc83660
Kiskotkd <116261484+Kiskotkd@users.noreply.github.com>
2023-07-06 10:53:08 +0200

    Delete Typescript directory

--- Typescript/ContactTest.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Person_1 = require("./Person");
-var Contact_1 = require("./Contact");
-var contacto = new Contact_1.Contact();
-var person1 = new Person_1.Person("Laia", 25, "Santa Catalina de Genoveva");
-contacto.people.push(person1);
-contacto.printCalendar();
