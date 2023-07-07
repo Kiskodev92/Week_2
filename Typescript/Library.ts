ebde9189a18b4e4d54a903e2a6e5c1b367972291
Kiskotkd <fjgp92@hotmail.com>
2023-07-07 12:38:31 +0200

    m

--- Typescript/Library.ts
+++ Typescript/Library.ts
@@ -25,7 +25,19 @@ class Library{
         return `Book 1
         ${libro1.toString()}`
     }
-    
+    getNumbersofbooks():number
+    {
+        return this.books.length
+    }
+    FindAutor(author: string): Book[] {
+        return this.books.filter(valor => valor.getAutor() == author)
+    }
 }
 let libro1 : Book = new Book ("El hobbit", 300, "aoaicj213", "Tolkien", "Salvat");
-console.log(libro1.toString());
+let libro2 : Book = new Book ("Juego de tronos", 660, "685861oausdh", "George R.R.Martin", "Planeta Agostini");
+let libro3 : Book = new Book ("Memorias de Idhun", 700, "6ca65s1ca", "Laura Gallego", "Salamandra");
+let libreria:Library = new Library([libro1,libro2,libro3],"La Habana","Rowling")
+
+console.log(libreria.toString());
+console.log(libreria.getNumbersofbooks());
+console.log(libreria.FindAutor("Tolkien"));
